be989310fd233b47fb18ae1435e1461cd0d2b996
Amodu Emmanuel <aWX536413@china.huawei.com>
2019-01-06 16:56:58 +0100

    service stub

---
+++ scripts/database/database.ts
@@ -0,0 +1,31 @@
+export class Database {
+    /**
+     * Class for database operations
+     */
+    constructor() {
+        
+    }
+    /**
+     * Open connection to the database
+     */
+    open(){
+
+    }
+    /**
+     * Close connection to the database
+     */
+    close() {
+
+    }
+
+    /**
+     * truncate a document
+     */
+    trunc_doc() {
+
+    }
+
+    create_doc(){
+
+    }
+}
---
+++ scripts/database/db_operation.ts
@@ -0,0 +1,26 @@
+import { Database } from './database';
+
+export class DbOprations extends Database {
+    /**
+     * Database operation class
+     */
+    constructor() {
+        super();
+    }
+
+    public create() {
+
+    }
+
+    public Read() {
+
+    }
+
+    public update() {
+
+    }
+
+    public delete() {
+
+    }
+}
